3ecf2f5781f58b758558efccec033b74191950d9
Philip Chukwuma <cchukwuma.p@gmail.com>
2022-12-02 17:10:00 +0000

    setup pages: splash, login and register using fragments

---
+++ app/src/main/java/uk/ac/tees/b1498820/wefix/activities/MainActivity.java
@@ -0,0 +1,50 @@
+package uk.ac.tees.b1498820.wefix.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import uk.ac.tees.b1498820.wefix.R;
+import uk.ac.tees.b1498820.wefix.adapters.AuthenticationPagerAdapter;
+import uk.ac.tees.b1498820.wefix.databinding.ActivityLoginBinding;
+import uk.ac.tees.b1498820.wefix.databinding.ActivityMainBinding;
+import uk.ac.tees.b1498820.wefix.fragments.LoginFragment;
+import uk.ac.tees.b1498820.wefix.fragments.RegisterFragment;
+import uk.ac.tees.b1498820.wefix.fragments.SplashFragment;
+
+public class MainActivity extends AppCompatActivity {
+ViewPager2 viewPager;
+ActivityMainBinding binding;
+View view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        view = binding.getRoot();
+        setContentView(view);
+
+        viewPager = binding.viewPager;
+
+        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager(), getLifecycle());
+        pagerAdapter.addFragment(new SplashFragment());
+        pagerAdapter.addFragment(new LoginFragment());
+        pagerAdapter.addFragment(new RegisterFragment());
+        viewPager.setAdapter(pagerAdapter);
+
+//         set handler to run task for specific time interval
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                pagerAdapter.removeFragment(0);
+                viewPager.setAdapter(pagerAdapter);
+            }
+        }, 2000);
+
+
+
+    }
+}
---
+++ app/src/main/java/uk/ac/tees/b1498820/wefix/adapters/AuthenticationPagerAdapter.java
@@ -0,0 +1,37 @@
+package uk.ac.tees.b1498820.wefix.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class AuthenticationPagerAdapter extends FragmentStateAdapter {
+    private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+    public AuthenticationPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        fragmentList.add(fragment);
+    }
+
+    public void removeFragment(int index) {
+        fragmentList.remove(index);
+    }
+}
